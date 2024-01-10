92edbe5c22165478129eed6ca30edc7d280753d9
Francesco <francescopaul789@gmail.com>
2024-01-10 01:42:10 +0100

    Tokenize src string

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
 # source-str --> Lexer (creates tokens) --> Creation of tree --> Tree parsing
 
-source_string = "12*(3+4)^2"
-
 class Lexer:
     # get tokens
     # recognize which tokens mark the beginning of new tokens
@@ -10,19 +8,31 @@ class Lexer:
         self.source_string = source_string
         self.tokens = []
         
-    def tokenize(self, source_string):
+    def tokenize(self, source_string, idx = 0):
         if len(source_string) <= 1:
             self.tokens.append(source_string)
             return
+        
+        if idx >= len(source_string): return 
 
-        self.tokens.append(source_string[0])
-        self.tokenize(source_string[1:len(source_string)])
-    
+        curr_val = source_string[idx] 
+        
+        if self.is_digit(curr_val):
+            self.tokenize(source_string, idx + 1)
+        else:
+            self.tokens.append(source_string[:idx]) # final number
+            self.tokens.append(source_string[idx]) # operator or parenthesis
+            self.tokenize(source_string[idx+1:], 0)
+
+    def is_digit(self, value):
+       return value not in ["+", "-", "*", "/", "(", ")", "^"] # they indicate start of new number
 
 class Parser:
     def __init__(self):
         pass
 
+
+source_string = "12*(3+4)^2"
 
 lexer = Lexer(source_string)
 lexer.tokenize(lexer.source_string)
