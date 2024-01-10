4ef025041dc6bc28320e22addf2a33074b1d4295
Francesco <francescopaul789@gmail.com>
2024-01-10 16:25:54 +0100

    (#1) Fix: use concat instead of slicing

--- main.py
+++ main.py
@@ -5,23 +5,27 @@ class Lexer:
     # get tokens
     # recognize which tokens mark the beginning of new tokens
     def __init__(self, src_str):
-        self.src_str = source_string
+        self.src_str = src_str 
         self.tokens = []
         
     def tokenize(self, src_str, idx = 0):
         if len(src_str) <= 1:
             self.tokens.append(src_str)
             return
-        
+
         if idx >= len(src_str): return 
 
         curr_val = src_str[idx] 
-        
+
         if self.is_digit(curr_val):
+            if len(self.tokens) > 0 and self.is_digit(self.tokens[-1]): 
+                self.tokens[-1] += curr_val
+            else:
+                self.tokens.append(curr_val)
+
             self.tokenize(src_str, idx + 1)
         else:
-            self.tokens.append(src_str[:idx]) # final number
-            self.tokens.append(src_str[idx]) # operator or parenthesis
+            self.tokens.append(curr_val) # operator or parenthesis
             self.tokenize(src_str[idx+1:], 0)
 
     def is_digit(self, value):
@@ -32,8 +36,9 @@ class Parser:
         pass
 
 
-src_str = "12*(3+4)^2"
 
+src_str = "12*(3+4)^2"
+src_str = "456 *((3-3)+2)-2"
 lexer = Lexer(src_str)
 lexer.tokenize(lexer.src_str)
 print(lexer.tokens)
