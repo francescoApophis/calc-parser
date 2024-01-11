1e2dfe2a6920528ab3822175a9b95d3536d29361
Francesco <francescopaul789@gmail.com>
2024-01-12 00:18:39 +0100

    Create EmptyParenthesisError

--- errors.py
+++ errors.py
@@ -7,3 +7,8 @@ class MissingOperandError(Exception):
     def __init__(self, a, b ):
         self.message = f"Missing operand between '{a}' and '{b}" 
         super().__init__(self.message)
+
+class EmptyParenthesisError(Exception):
+    def __init__(self, idx):
+        self.message = f"Empty parenthesis at index '{idx}'" 
+        super().__init__(self.message)
