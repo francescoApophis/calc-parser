bb3d2cc817d886a94f774883c67879100befb9bd
Francesco <francescopaul789@gmail.com>
2024-01-12 00:10:06 +0100

    Create MissingOperandError

--- errors.py
+++ errors.py
@@ -1,4 +1,9 @@
 class NoOperatorFoundError(Exception):
     def __init__(self):
-        super().__init__("Input is just number, no operation to perform")
-        
+        self.message = "Input is just number, no operation to perform"
+        super().__init__(self.message)
+
+class MissingOperandError(Exception):
+    def __init__(self, a, b ):
+        self.message = f"Missing operand between '{a}' and '{b}" 
+        super().__init__(self.message)
