e0796f8162c06eb2c8508a68cf7a0dbc20b213c1
Francesco <francescopaul789@gmail.com>
2024-01-12 02:03:46 +0100

    Add MismatchedParenthesisError

--- errors.py
+++ errors.py
@@ -6,6 +6,7 @@ class NoOperatorFoundError(Exception):
 class MissingOperandError(Exception):
     def __init__(self, idx, a = None, b = None):
         empty = ""
+        # a/b are none if except occurred at either start or end of str
         self.message = f"Missing operand between '{empty if a is None else a}' and '{empty if b is None else b}' at index {idx}" 
         super().__init__(self.message)
 
@@ -19,4 +20,11 @@ class  OppositeParenthesisError(Exception):
         self.message = f"Opposite parenthesis with no operator between at index '{idx}'" 
         super().__init__(self.message)
 
+
+class MismatchedParenthesisError(Exception):
+    def __init__(self, a, b):
+        type_par = "(" if a > b else ")"
+        excess_par = a-b if a > b else b - a
+        self.message = f"Mismatched parenthesis: {excess_par} too many '{type_par}' parenthesis" 
+        super().__init__(self.message)
 
