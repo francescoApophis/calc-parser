86f01a66e78d6dc25e9c05269d4081d95a6b91e3
Francesco <francescopaul789@gmail.com>
2024-01-30 22:02:38 +0100

    IMPORTANT: implement opretor precedence parser (bottom-up) *eventual float perands are casted to int*

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Parser:
     
     def parse_primary(self):   
         # returns the rhs of an operator when one is meet
-        # 'primary' follows the naming on the wikipedia article about 
+        # 'primary' follows the naming convention on the wikipedia article about 
         # -- operator precedence parser --
         return self.tokens[self.counter + 1]
     
@@ -35,9 +35,34 @@ class Parser:
     def parse(self, lhs, min_prec):
         nt = self.peek()
 
+        while nt is not None and is_oper(nt) and calc_prec(nt) >= min_prec:
+            op = nt 
+            self.increment_counter()
+            rhs = self.parse_primary()
+            self.increment_counter()
+            nt = self.peek()
+            
+            # get result of the operation ahead *FIRST* if the the operator has higher precedence
+            while nt is not None and is_oper(nt) and calc_prec(nt) > calc_prec(op):
+                prec =  calc_prec(op) + 1 if calc_prec(nt) > calc_prec(op) else 0
+                rhs = self.parse(rhs, prec)
+                nt = self.peek()
+            
+            
+            print(lhs, rhs)
+            if op == '+':
+                lhs = int(lhs) + int(rhs)
+            elif op == '-':
+                lhs = int(lhs) - int(rhs)
+            elif op == '*':
+                lhs = int(lhs) * int(rhs)
+            elif op == '/':
+                lhs = int(lhs) / int(rhs)
 
+        return lhs
+        
 
-src_str = "45-23"
+src_str = "7 + 2 * 3 + 1 / 10"
 parser = Parser(src_str)
 tokens = parser.tokens
 print("result: ", parser.parse(parser.tokens[0], 0))
