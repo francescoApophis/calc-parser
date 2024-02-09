156fd80aa557138314a9cd844f7943ce453ea942
Francesco <francescopaul789@gmail.com>
2024-02-09 02:21:14 +0100

    Add abstract syntax tree generation (BUGGED, some operations gets skipped)

--- main.py
+++ main.py
@@ -59,19 +59,46 @@ class Parser:
             
             # get result of the operation ahead *FIRST* if the the operator has higher precedence
             while nt is not None and is_oper(nt) and calc_prec(nt) > calc_prec(op):
-                prec =  calc_prec(op) + 1 if calc_prec(nt) > calc_prec(op) else 0
-                rhs = self.parse(rhs, prec)
+                new_min_prec =  calc_prec(op) + 1 if calc_prec(nt) > calc_prec(op) else 0
+                rhs = self.parse(rhs, new_min_prec)
                 nt = self.peek()
             
             lhs = self.calculate(lhs, op, rhs)
         return lhs
+
+    def generate_ast(self,lhs, min_prec = 0, higher = False):
+
+        nt = self.peek()
+        while nt is not None and is_oper(nt) and calc_prec(nt) >= min_prec:
+            op = nt 
+            self.increment_counter()
+            rhs = self.parse_primary()
+            self.increment_counter()
+            nt = self.peek()
+            
+            lhs['op'] = op
+            lhs['rhs'] = rhs
+            
+            while nt is not None and is_oper(nt):
+                if calc_prec(nt) > calc_prec(op):
+                    new_min_prec = calc_prec(op) + 1 if calc_prec(nt) > calc_prec(op) else 0 
+                    rhs = self.generate_ast({'lhs':rhs}, new_min_prec, True)
+                    lhs['rhs'] = rhs
+                    nt = self.peek()
+                else:
+                    higher = False
+                    break
+
+            if nt is not None and calc_prec(nt) <= calc_prec(op) and not higher:
+                node_copy = lhs 
+                lhs = {'lhs': node_copy}
+            
+        return lhs
         
 if __name__ == '__main__':
-    src_str = "" # insert expression
+    src_str = "1 / 7 * 4 + 3 / 6 * 5 - 7 " # insert expression
     parser = Parser(src_str)
     print("result:", parser.parse(parser.tokens[0], 0))
-
-
-
-
+    parser.counter = 0
+    print("AST:", parser.generate_ast({'lhs': parser.tokens[0]}, 0))
 
