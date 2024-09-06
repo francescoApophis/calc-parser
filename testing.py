62583401ab832e9848f1ac40a050c8cefb7f1dce
Francesco <francescopaul789@gmail.com>
2024-09-06 15:52:01 +0200

    IMPORTANT: fix random generated expressions (READ MSG)
    Sub-expressions with parentheses in random expressions
    now can actually have multiple operations, unlike before
    where they would only have one '...-(a+b)*...'

--- testing.py
+++ testing.py
@@ -4,12 +4,13 @@ import unittest
 from random import randint, choice
 import re
 
+opers = ['+', '/', '-', '*']
+tests_count = 10 * 1000
 
 def rand_num(expr, end_rand = 100000):
     num = str(randint(1, end_rand))
     return num
 
-opers = ['+', '/', '-', '*']
 
 ''' at the moment it doesn't generate exprs with: 
     - negative nums: -1 + (-1 * 5)
@@ -18,14 +19,14 @@ opers = ['+', '/', '-', '*']
 def gen_rand_exprs(max_opers_in_sub_expr = 5, nesting_level = 1, curr_level = 1):
     expr = ''
     if curr_level >= nesting_level:
-        for i in range(randint(2, max_opers_in_sub_expr)):
-            pars = randint(0, 1)
-            expr += '(' if pars else ''
+        pars = randint(0, 1) 
+        expr += '(' if pars else ''
+        for j in range(randint(2, max_opers_in_sub_expr)):
             expr += rand_num(expr)
             expr += choice(opers)
             expr += rand_num(expr) if pars else ''
-            expr += ')' if pars else ''
-            expr += choice(opers) if pars else ''
+        expr += ')' if pars else ''
+        expr += choice(opers) if pars else ''
 
         if expr[-1] in opers:
             expr = expr[:-1]
@@ -42,55 +43,45 @@ def gen_rand_exprs(max_opers_in_sub_expr = 5, nesting_level = 1, curr_level = 1)
     # despite the check in rand_num() 
     # for now this is how I deal with it
     if '/0' in expr:
-        expr = expr.replace('/0', '/' + randint(1, 100000))
+        expr = expr.replace('/0', '/' + randint(1, 10))
     return expr
 
 
 # @unittest.skip('not needed')
 class TestRandGenExpressions(unittest.TestCase):
     def test_rand_expressions_are_correct(self):
-        for i in range(10 * 1000):
+        for i in range(tests_count):
             self.assertTrue(Lexer.is_invalid(gen_rand_exprs(nesting_level = randint(1, 10))))
         
 
 class TestParser(unittest.TestCase):
     def test_gen_trees_from_epxrs(self):
-        for i in range(10 * 1000):
+        for i in range(tests_count):
             expr = gen_rand_exprs(nesting_level = randint(1, 10))
             parser = Parser(expr)
-            py_eval_zero_div_err, parser_zero_div_err = None, None
             py_result, parser_result = None, None
 
             try: 
                 py_result = eval(expr)
             except ZeroDivisionError as inst: 
-                py_eval_zero_div_err = inst.args
-            try: 
-                parser_result = parser.calc_from_tree()
-            except ZeroDivisionError as inst: 
-                parser_zero_div_err = inst.args
+                py_result = 'undefined'
 
-            self.assertEqual(py_eval_zero_div_err, parser_zero_div_err)
+            parser_result = parser.calc_from_tree()
             self.assertEqual(parser_result, py_result)
         
     def test_exprs(self):
-        for i in range(10 * 1000):
+        for i in range(tests_count):
             expr = gen_rand_exprs(nesting_level = randint(1, 10))
             parser = Parser(expr)
-            py_eval_zero_div_err, parser_zero_div_err = None, None
             py_result, parser_result = None, None
 
             try: 
                 py_result = eval(expr)
-            except ZeroDivisionError as inst: 
-                py_eval_zero_div_err = inst.args
-            try: 
-                parser_result = parser.parse_and_calc()
-            except ZeroDivisionError as inst: 
-                parser_zero_div_err = inst.args
+            except ZeroDivisionError: 
+                py_result = 'undefined'
 
-            self.assertEqual(py_eval_zero_div_err, parser_zero_div_err)
-            self.assertEqual(parser_result, py_result)
+            parser_result = parser.parse_and_calc()
+            self.assertEqual(parser_result, py_result, msg= f'\n{expr}')
 
 def suite():
     suite = unittest.TestSuite()
